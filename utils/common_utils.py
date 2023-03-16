2543a784056515b103c22a0a636c04a5e9435273
Jatin Akad <jatin.akad@gmail.com>
2023-03-16 13:37:13 +0530

    final

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -69,16 +69,17 @@ def plot_image_grid(images_np, nrow =8, factor=1, interpolation='lanczos'):
         interpolation: interpolation used in plt.imshow
     """
     n_channels = max(x.shape[0] for x in images_np)
-    assert (n_channels == 3) or (n_channels == 1), "images should have 1 or 3 channels"
+    assert (n_channels == 3) or (n_channels == 1) or (n_channels == 4), "images should have 1 or 3 channels"
     
     images_np = [x if (x.shape[0] == n_channels) else np.concatenate([x, x, x], axis=0) for x in images_np]
-
+    #images_np = [np.concatenate([x, x, x, x], axis=0) for x in images_np]
+    
     grid = get_image_grid(images_np, nrow)
     
     plt.figure(figsize=(len(images_np) + factor, 12 + factor))
     
     if images_np[0].shape[0] == 1:
-        plt.imshow(grid[0], cmap='gray', interpolation=interpolation)
+        plt.imshow(grid[0], interpolation=interpolation) #cmap='gray'
     else:
         plt.imshow(grid.transpose(1, 2, 0), interpolation=interpolation)
     
